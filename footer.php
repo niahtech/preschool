eb8c68e30ea1aeaca5c1e383fac497a8a53aba9a
Ekundayo Israel <isekundayo700@gmail.com>
2023-03-03 09:40:36 +0100

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-
-      <script src="assets/js/jquery-3.6.0.min.js"></script>
-      <script src="assets/js/popper.min.js"></script>
-      <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-      <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-      <script src="assets/plugins/apexchart/apexcharts.min.js"></script>
-      <script src="assets/plugins/apexchart/chart-data.js"></script>
-      <script src="assets/js/script.js"></script>
