13bc19674a96fd47f439ee029595c5732b84988b
i-ekundayo <isekundayo700@gmail.com>
2023-03-18 16:52:53 +0100

    initial commit

---
+++ student-list.php
@@ -0,0 +1,152 @@
+<?php
+   include 'lecturer-header.php';
+   $students = $db->query("SELECT * FROM students");
+?>
+
+         <div class="page-wrapper">
+            <div class="content container-fluid">
+               <div class="page-header">
+                  <div class="row align-items-center">
+                     <div class="col">
+                        <h3 class="page-title">Students</h3>
+                        <ul class="breadcrumb">
+                           <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                           <li class="breadcrumb-item active">Students</li>
+                        </ul>
+                     </div>
+                     <div class="col-auto text-right float-right ml-auto">
+                        <a href="#" class="btn btn-outline-primary mr-2"><i class="fas fa-download"></i> Download</a>
+                        <a href="add-student.html" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-sm-12">
+                     <div class="card card-table">
+                        <div class="card-body">
+                           <div class="table-responsive">
+                              <table class="table table-hover table-center mb-0 datatable">
+                                 <thead>
+                                    <tr>
+                                       <th>ID</th>
+                                       <th>Name</th>
+                                       <th>Class</th>
+                                       <th>DOB</th>
+                                       <th>Parent Name</th>
+                                       <th>Mobile Number</th>
+                                       <th>Address</th>
+                                    </tr>
+                                 </thead>
+                                 <tbody>
+                                    <?php foreach($students as $student): ?>
+                                    <tr>
+                                       <td><?= $student['studentId']; ?></td>
+                                       <td>
+                                          <h2 class="table-avatar">
+                                             <a href="#" data-id="<?= $student['id']; ?>" data-toggle="modal" data-target="#student-<?= $student['id']; ?>" class="avatar avatar-sm mr-2 list"><img class="avatar-img rounded-circle" src="assets/img/profiles/<?= $student['image']; ?>" alt="User Image"></a>
+                                             <a href="#" data-id="<?= $student['id']; ?>" class="list" data-toggle="modal" data-target="#student-<?= $student['id']; ?>"><?= $student['firstName']; ?></a>
+                                          </h2>
+                                       </td>
+                                       <td><?= $student['department']; ?></td>
+                                       <td><?= $student['dob']; ?></td>
+                                       <td><?= $student['fatherName']; ?></td>
+                                       <td><?= $student['mobileNumber']; ?></td>
+                                       <td><?= $student['permanentAddress']; ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                 </tbody>
+                              </table>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <footer>
+               <p>Copyright © 2020 Dreamguys.</p>
+            </footer>
+         </div>
+      </div>
+
+      <?php foreach($students as $student) :?>
+      <div class="modal fade" tabindex="-1" aria-labelledby="studentDetailsLabel" aria-hidden="true" id="student-<?= $student['id']; ?>">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <div class="modal-title text-center h3">Student Details</div>
+                  <button class="btn btn-danger close" data-dismiss="modal">&times;</button>
+               </div>
+               <div class="modal-body">
+                  <ul class="breadcrumb">
+                     <li class="breadcrumb-item"><a href="students.php">Student</a></li>
+                     <li class="breadcrumb-item active">Student Details</li>
+                  </ul>
+                  <div class="card">
+                     <div class="card-body">
+                        <div class="row">
+                           <div class="col-md-12">
+                              <div class="about-info">
+                                 <h4>About Me</h4>
+                                 <div class="media mt-3">
+                                    <img src="assets/img/user.jpg" class="mr-3 img-fluid w-50" alt="...">
+                                    <div class="media-body w-50">
+                                       <ul>
+                                          <li>
+                                             <span class="title-span">Full Name: </span>
+                                             <span class="info-span"><?= $student['lastName'].' '. $student['firstName'];?></span>
+                                          </li>
+                                          <li>
+                                             <span class="title-span">Department: </span>
+                                             <span class="info-span"><?= $student['department']; ?></span>
+                                          </li>
+                                          <li>
+                                             <span class="title-span">Mobile: </span>
+                                             <span class="info-span"><?= $student['mobileNumber']; ?></span>
+                                          </li>
+                                          <li>
+                                             <span class="title-span">Email: </span>
+                                             <span class="info-span"><a href="https://preschool.dreamguystech.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="7216131b010b32151f131b1e5c111d1f">[email&#160;protected]</a></span>
+                                          </li>
+                                          <li>
+                                             <span class="title-span">Gender: </span>
+                                             <span class="info-span"><?= $student['gender']; ?></span>
+                                          </li>
+                                          <li>
+                                             <span class="title-span">DOB: </span>
+                                             <span class="info-span"><?= $student['dob']; ?></span>
+                                          </li>
+                                       </ul>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <form method="GET" action="student-result.php">
+                     <input type="hidden" name="bookId" id="bookId" value="">
+                     <button class="btn btn-primary" type="submit" name="resultUpdate" value="<?= $student['department']; ?>">Update Result</button>
+                  </form>
+                  
+               </div>
+            </div>
+         </div>
+      </div>
+      <?php endforeach; ?>
+
+
+      <script src="assets/js/jquery-3.6.0.min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+      <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+      <script src="assets/plugins/datatables/datatables.min.js"></script>
+      <script src="assets/js/script.js"></script>
+      <script>
+         $('.list').click(function(e) {
+            var bookId = $(this).data('id');
+            $(".modal-body #bookId").val(bookId);
+         });
+      </script>
+   </body>
+</html>
---
+++ student-result.js
@@ -0,0 +1,62 @@
+var score = document.querySelectorAll('.score');
+var grade = document.querySelectorAll('.grade');
+var unit = document.querySelectorAll('.unit');
+
+var sum = 0;
+for(let i=0; i<grade.length; i++){
+    score[i].addEventListener('keyup', (e) => {
+        let value = score[i].value;
+        if(score[i].value.length !== 0){
+            switch(true) {
+                case value < 45:
+                    grade[i].value = 'F';
+                    break;
+                case value < 50:
+                    grade[i].value = 'D';
+                    break;
+                case value < 60:
+                    grade[i].value = 'C';
+                    break;
+                case value < 70:
+                    grade[i].value = 'B';
+                    break;
+                case value <= 100:
+                    grade[i].value = 'A';
+                    break;
+                default:
+                    alert('The score cannot be greater than 100');
+            }
+        }
+        else{
+            grade[i].value = '';
+        }
+    })
+}
+
+var TQP = 0, TLU = 0, QP;
+document.querySelector('.GPA').addEventListener('click', (e) => {
+    for(let i=0; i<grade.length; i++){
+        switch(grade[i].value){
+            case 'F':
+                QP = parseInt(unit[i].value) * 0;
+                break;
+            case 'D':
+                QP = parseInt(unit[i].value) * 2;
+                break;
+            case 'C':
+                QP = parseInt(unit[i].value) * 3;
+                break;
+            case 'B':
+                QP = parseInt(unit[i].value) * 4;
+                break;
+            case 'A':
+                QP = parseInt(unit[i].value) * 5;
+                break;
+        }
+        TQP += QP;
+        TLU += parseInt(unit[i].value);
+    }
+    var GPA = TQP/TLU;
+    document.querySelector('.currentGPA').value = GPA.toFixed(2);
+    // var CGPA = 
+})
