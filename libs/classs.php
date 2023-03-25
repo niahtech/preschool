fc4a4869a510da6ed3cdf41174d2ec43c6664d2b
Ekundayo Israel <isekundayo700@gmail.com>
2023-03-25 11:24:12 +0100

    Update classs.php

--- libs/classs.php
+++ libs/classs.php
@@ -51,17 +51,33 @@ class lecturer {
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
         $password = $_POST['password'];
 
-        $sql = $db->query("SELECT email,password FROM lecturers WHERE email = '$email' LIMIT 1");
-        $result = $sql->fetch_assoc();
-        if(mysqli_num_rows($sql) > 0){
-            if(!password_verify($password, $result['password'])) {
-                $loginErr = 'Check your Email or Password';
+        if($user == 'Lecturer'){
+            $sql = $db->query("SELECT email,password FROM lecturers WHERE email = '$email' LIMIT 1");
+            $result = $sql->fetch_assoc();
+            if(mysqli_num_rows($sql) > 0){
+                if(!password_verify($password, $result['password'])) {
+                    $loginErr = 'Check your Email or Password';
+                }else {
+                    $_SESSION['email'] = $email;
+                    header('Location: teacher-dashboard.php');
+                }
             }else {
-                $_SESSION['email'] = $email;
-                header('Location: teacher-dashboard.php');
+                $loginErr = 'You are not a registered user';
             }
-        }else {
-            $loginErr = 'You are not a registered user';
+        }
+        else{
+            $sql = $db->query("SELECT email,password FROM students WHERE email = '$email' LIMIT 1");
+            $result = $sql->fetch_assoc();
+            if(mysqli_num_rows($sql) > 0){
+                if(!password_verify($password, $result['password'])) {
+                    $loginErr = 'Check your Email or Password';
+                }else {
+                    $_SESSION['email'] = $email;
+                    header('Location: student-dashboard.php');
+                }
+            }else {
+                $loginErr = 'You are not a registered user';
+            } 
         }
     }
 
@@ -73,7 +89,7 @@ class lecturer {
         $dob = filter_input(INPUT_POST, 'dob', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $mobile = filter_input(INPUT_POST, 'mobile', FILTER_SANITIZE_NUMBER_INT);
         $qualification = filter_input(INPUT_POST, 'qualification', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $course= filter_input(INPUT_POST, 'course', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $course= implode(',', filter_input(INPUT_POST, 'course', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
         $experience = filter_input(INPUT_POST, 'experience', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -83,6 +99,10 @@ class lecturer {
         $zipCode = filter_input(INPUT_POST, 'zipCode', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $country = filter_input(INPUT_POST, 'country', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+        if(empty($course)){
+            $courseErr = 'Please select a course';
+        }
+        
         // Image handling
         $allowedExt = ['png', 'jpg', 'jpeg', 'gif'];
         if(!empty($_FILES['image']['name'])){
